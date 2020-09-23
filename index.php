512e22739340a2e52c90af14f6cc2db224d534b8
yefangyong <1281074511@qq.com>
2020-09-23 15:09:45 +0800

    代码格式调整

--- index.php
+++ index.php
@@ -6,26 +6,26 @@
  * 3.启动框架
  */
 
-define("IMOOC",realpath("./"));
-define("CORE",IMOOC."/core");
-define("APP",IMOOC."/app");
-define('MODULE','app');
-define("DEBUG",true);
+define("IMOOC", realpath("./"));
+define("CORE", IMOOC . "/core");
+define("APP", IMOOC . "/app");
+define('MODULE', 'app');
+define("DEBUG", true);
 
 include './vendor/autoload.php';
-if(DEBUG) {
-    $whoops = new \Whoops\Run;
+if (DEBUG) {
+    $whoops  = new \Whoops\Run;
     $options = new \Whoops\Handler\PrettyPageHandler();
     $options->setPageTitle('框架出错啦');
     $whoops->pushHandler($options);
     $whoops->register();
-    ini_set('display_error','on');
-}else{
-    ini_set('display_error','off');
+    ini_set('display_error', 'on');
+} else {
+    ini_set('display_error', 'off');
 }
-include CORE.'/common/function.php';
+include CORE . '/common/function.php';
 
-include CORE.'/imooc.php';
+include CORE . '/imooc.php';
 //自动注册类，找不到类库，自动调用方法
 spl_autoload_register('\core\imooc::load');
 
